2d953910cced557979effe6ea4f445210d0a0e05
wilkon <tb.wilkon@gmail.com>
2020-04-27 03:23:00 -0400

    MenuScreen => creating stage and linking input; set max framerate

--- core/src/com/box2d/game/views/MenuScreen.java
+++ core/src/com/box2d/game/views/MenuScreen.java
@@ -1,13 +1,28 @@
 package com.box2d.game.views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.box2d.game.Box2dTutorial;
 
 public class MenuScreen implements Screen {
     private Box2dTutorial parent;
+    private Stage stage;
 
     public MenuScreen(Box2dTutorial parent){
         this.parent = parent;
+        this.stage = new Stage(new ScreenViewport());
+
+        // any input will send a request from stage for response
+        Gdx.input.setInputProcessor(stage);
+
+        // act asks our stage to perform as we "draw" the curtains
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+                    // ^ maximum frame rate with this MathMin function
+
+        
+        stage.draw();
     }
 
     @Override
