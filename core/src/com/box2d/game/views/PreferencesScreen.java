8dac820e03d712d28c48f0800384147f9e8db7ca
wilkon <tb.wilkon@gmail.com>
2020-04-27 13:39:11 -0400

    Preferences => stage, table, and skin loaded

--- core/src/com/box2d/game/views/PreferencesScreen.java
+++ core/src/com/box2d/game/views/PreferencesScreen.java
@@ -1,23 +1,41 @@
 package com.box2d.game.views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.box2d.game.Box2dTutorial;
 
 public class PreferencesScreen implements Screen {
     private Box2dTutorial parent;
+    private Stage stage;
 
     public PreferencesScreen(Box2dTutorial parent){
         this.parent = parent;
+        stage = new Stage(new ScreenViewport());
     }
 
     @Override
     public void show() {
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/neon-ui.json"));
 
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+        stage.draw();
     }
 
     @Override
