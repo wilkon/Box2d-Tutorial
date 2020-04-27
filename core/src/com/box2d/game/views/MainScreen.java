0eda60f59138bd2903bba7f85d32afe32ef5fd47
wilkon <tb.wilkon@gmail.com>
2020-04-27 18:28:49 -0400

    MainMenu => implemented model, debugRendering, ortho camera

--- core/src/com/box2d/game/views/MainScreen.java
+++ core/src/com/box2d/game/views/MainScreen.java
@@ -1,13 +1,27 @@
 package com.box2d.game.views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.box2d.game.Box2dTutorial;
+import com.box2d.game.models.Box2dModel;
 
 public class MainScreen implements Screen {
     private Box2dTutorial parent;
 
+    Box2dModel model;
+    OrthographicCamera cam;
+
+    Box2DDebugRenderer debugRenderer;
+
     public MainScreen(Box2dTutorial parent){
         this.parent = parent;
+        model = new Box2dModel();
+        cam = new OrthographicCamera(32, 24);
+        debugRenderer = new Box2DDebugRenderer(true, true, true,
+                true,true, true);
     }
 
     @Override
@@ -17,7 +31,11 @@ public class MainScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        //we will use this to move our game logic
+        model.logicStep(delta);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        debugRenderer.render(model.world, cam.combined);
     }
 
     @Override
