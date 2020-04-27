5fea6bf6394a35edb2e11b6afe15426a9cdc326e
wilkon <tb.wilkon@gmail.com>
2020-04-27 13:21:59 -0400

    AppPreferences => implemented update and get preferences

--- core/src/com/box2d/game/preferences/AppPreferences.java
+++ core/src/com/box2d/game/preferences/AppPreferences.java
@@ -1,4 +1,52 @@
 package com.box2d.game.preferences;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class AppPreferences {
+    private static final String PREF_MUSIC_VOLUME = "volume";
+    private static final String PREF_MUSIC_ENABLED = "music.enabled";
+    private static final String PREF_SOUND_ENABLED = "sound.enabled";
+    private static final String PREF_SOUND_VOL = "sound";
+    private static final String PREFS_NAME = "box2d";
+
+    protected Preferences getPrefs(){
+        return Gdx.app.getPreferences(PREFS_NAME);
+    }
+
+    public boolean isSoundEffectsEnabled(){
+        return getPrefs().getBoolean(PREF_SOUND_ENABLED);
+    }
+
+    public void setSoundEffectEnabled(boolean on){
+        getPrefs().putBoolean(PREF_SOUND_ENABLED, on);
+        getPrefs().flush();
+    }
+
+    public boolean isMusicEnabled(){
+        return getPrefs().getBoolean(PREF_MUSIC_ENABLED);
+    }
+
+    public void setMusicEnabled(boolean on){
+        getPrefs().putBoolean(PREF_MUSIC_ENABLED, on);
+        getPrefs().flush();
+    }
+
+    public float getPrefMusicVolume() {
+        return getPrefs().getFloat(PREF_MUSIC_VOLUME, 0.5f);
+    }
+
+    public void setMusicVolume(float volume){
+        getPrefs().putFloat(PREF_MUSIC_VOLUME, volume);
+        getPrefs().flush();
+    }
+
+    public float getSoundVolume(){
+        return getPrefs().getFloat(PREF_SOUND_VOL);
+    }
+
+    public void setSoundEnabled(boolean on){
+        getPrefs().putBoolean(PREF_SOUND_ENABLED, on);
+        getPrefs().flush();
+    }
 }
