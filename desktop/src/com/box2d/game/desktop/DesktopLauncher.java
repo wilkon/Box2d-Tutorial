5204a1ea8d726b28d3567aa8709507b006e533e2
wilkon <tb.wilkon@gmail.com>
2020-04-27 03:38:52 -0400

    Gradle => updated build script/desktop launcher for LWJGL3

--- desktop/src/com/box2d/game/desktop/DesktopLauncher.java
+++ desktop/src/com/box2d/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.box2d.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.box2d.game.Box2dTutorial;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Box2dTutorial(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new Box2dTutorial(), config);
 	}
 }
