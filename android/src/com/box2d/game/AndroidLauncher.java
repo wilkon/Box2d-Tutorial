192b5b3e13cb4b4a5eba619af326c1de8c3fc62b
wilkon <tb.wilkon@gmail.com>
2020-04-27 02:27:09 -0400

    base gdx proj

---
+++ android/src/com/box2d/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.box2d.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.box2d.game.Box2dTutorial;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Box2dTutorial(), config);
+	}
+}
---
+++ desktop/src/com/box2d/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.box2d.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.box2d.game.Box2dTutorial;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Box2dTutorial(), config);
+	}
+}
