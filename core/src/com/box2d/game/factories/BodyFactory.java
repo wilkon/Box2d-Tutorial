abbeb695fd503df7816a58ddda4585b7387f1ff5
wilkon <tb.wilkon@gmail.com>
2020-04-28 10:32:27 -0400

    BodyFactory => singleton factory

---
+++ core/src/com/box2d/game/factories/BodyFactory.java
@@ -0,0 +1,22 @@
+package com.box2d.game.factories;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class BodyFactory {
+    private World world;
+
+    private static BodyFactory thisInstance;
+
+    private BodyFactory(World world){
+        this.world = world;
+    }
+
+    public static BodyFactory getInstance(World world){
+        if(thisInstance == null){
+            thisInstance = new BodyFactory(world);
+        }
+        return thisInstance;
+    }
+
+
+}
