d6fc4e6527f0d5f9f59af9e9779e7b717cc4b421
wilkon <tb.wilkon@gmail.com>
2020-04-28 10:42:38 -0400

    BodyFactory => makeFixture creating fixture definitions based on material

--- core/src/com/box2d/game/factories/BodyFactory.java
+++ core/src/com/box2d/game/factories/BodyFactory.java
@@ -1,8 +1,15 @@
 package com.box2d.game.factories;
 
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class BodyFactory {
+    public static final int STEEL = 0;
+    public static final int WOOD = 1;
+    public static final int RUBBER = 2;
+    public static final int STONE = 3;
+
     private World world;
 
     private static BodyFactory thisInstance;
@@ -18,5 +25,38 @@ public class BodyFactory {
         return thisInstance;
     }
 
+    public static FixtureDef makeFixture(int material, Shape shape){
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+
+        switch(material){
+            case STEEL:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0.3f;
+                fixtureDef.restitution = 0.1f;
+                break;
+            case WOOD:
+                fixtureDef.density = 0.5f;
+                fixtureDef.friction = 0.7f;
+                fixtureDef.restitution = 0.3f;
+                break;
+            case RUBBER:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0f;
+                fixtureDef.restitution = 1f;
+                break;
+            case STONE:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0.9f;
+                fixtureDef.restitution = 0.01f;
+                break;
+            default:
+                fixtureDef.density = 7f;
+                fixtureDef.friction = 0.5f;
+                fixtureDef.restitution = 0.3f;
+        }
+
+        return fixtureDef;
+    }
 
 }
