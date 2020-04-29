2c8c2e01b11a8468e98376247d6d9e7a2c3e70c1
wilkon <tb.wilkon@gmail.com>
2020-04-29 16:55:00 -0400

    AssetLoader => created with enemy and player image loadouts

--- core/src/com/box2d/game/loaders/AssetLoader.java
+++ core/src/com/box2d/game/loaders/AssetLoader.java
@@ -1,4 +1,16 @@
 package com.box2d.game.loaders;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+
 public class AssetLoader {
+    public final AssetManager manager = new AssetManager();
+
+    public final String playerImage = "images/player.png";
+    public final String enemyImage = "images/enemy.png";
+
+    public void queueAddImages(){
+        manager.load(playerImage, Texture.class);
+        manager.load(enemyImage, Texture.class);
+    }
 }
