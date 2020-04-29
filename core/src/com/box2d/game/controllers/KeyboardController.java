02e96e1795a518acc176bab35b097cc09f93b544
wilkon <tb.wilkon@gmail.com>
2020-04-29 12:47:35 -0400

    KeyboardController => including Mouse inputs

--- core/src/com/box2d/game/controllers/KeyboardController.java
+++ core/src/com/box2d/game/controllers/KeyboardController.java
@@ -1,11 +1,16 @@
 package com.box2d.game.controllers;
 
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Vector2;
 
 import static com.badlogic.gdx.Input.Keys.*;
 
 public class KeyboardController implements InputProcessor {
     public boolean left, right, up, down;
+
+    public boolean isMouse1Down, isMouse2Down, isMouse3Down;
+    public boolean isDragged;
+    public Vector2 mouseLocation;
 
     @Override
     public boolean keyDown(int keycode) {
@@ -60,22 +65,46 @@ public class KeyboardController implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if(button == 0){
+            isMouse1Down = true;
+        }else if(button == 1){
+            isMouse2Down = true;
+        }else if(button == 2){
+            isMouse3Down = true;
+        }
+        mouseLocation.x = screenX;
+        mouseLocation.y = screenY;
         return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        isDragged = false;
+        if(button == 0){
+            isMouse1Down = false;
+        }else if(button == 1){
+            isMouse2Down = false;
+        }else if(button == 2){
+            isMouse3Down = false;
+        }
+        mouseLocation.x = screenX;
+        mouseLocation.y = screenY;
         return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        isDragged = true;
+        mouseLocation.x = screenX;
+        mouseLocation.y = screenY;
         return false;
     }
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        return false;
+        mouseLocation.x = screenX;
+        mouseLocation.y = screenY;
+        return true;
     }
 
     @Override
