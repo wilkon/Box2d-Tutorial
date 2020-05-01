4c6a7613388162a0b091b9f5b20417e8cc49d83d
wilkon <tb.wilkon@gmail.com>
2020-04-30 23:09:14 -0400

    RenderingSystem => gathering components to render entities in accordnace to depth

--- core/src/com/box2d/game/ces/systems/RenderingSystem.java
+++ core/src/com/box2d/game/ces/systems/RenderingSystem.java
@@ -1,9 +1,16 @@
 package com.box2d.game.ces.systems;
 
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.SortedIteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.box2d.game.ces.components.TextureComponent;
+import com.box2d.game.ces.components.TransformComponent;
 
 import java.util.Comparator;
 
@@ -18,12 +25,109 @@ public class RenderingSystem extends SortedIteratingSystem {
     //ratio for converting pixels to meters
     public static final float PIXELS_TO_METERS = 1.0f / PPM;
 
-    public RenderingSystem(Family family, Comparator<Entity> comparator) {
-        super(family, comparator);
+    //screen width in meters
+    private static Vector2 meterDimensions = new Vector2();
+    private static Vector2 pixelDimensions = new Vector2();
+
+    public static Vector2 getScreenSizeInMeters(){
+        meterDimensions.set(
+                Gdx.graphics.getWidth() * PIXELS_TO_METERS,
+                Gdx.graphics.getHeight() * PIXELS_TO_METERS);
+        return meterDimensions;
+    }
+
+    public static Vector2 getScreenSizeInPixels(){
+        pixelDimensions.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        return pixelDimensions;
+    }
+
+    public static float pixelsToMeters(float pixelValue){
+        return pixelValue * PIXELS_TO_METERS;
+    }
+
+    // reference to our sprite batch
+    private SpriteBatch batch;
+
+    // array used to allow sorting of images allowing us to draw images on top of each other
+    private Array<Entity> renderQueue;
+
+    // comparator that allows us to sort images base don the Z position of the transformComponent
+    private Comparator<Entity> comparator;
+
+    // reference to our camera
+    private OrthographicCamera camera;
+
+    // component mappers to retrieve components from entities
+    private ComponentMapper<TextureComponent> textureMapper;
+    private ComponentMapper<TransformComponent> transformMapper;
+
+    @SuppressWarnings("unchecked")
+    public RenderingSystem(SpriteBatch batch) {
+
+        //get all entities with a transformComponent and TextureComponent
+        super(Family.all(TransformComponent.class, TextureComponent.class).get(), new ZComparator());
+
+        //craeetes out componentMappers
+        textureMapper = ComponentMapper.getFor(TextureComponent.class);
+        transformMapper = ComponentMapper.getFor(TransformComponent.class);
+
+        //create the array for sorting entities
+        renderQueue = new Array<Entity>();
+
+        //set our batch to the one supploed in the constructor
+        this.batch = batch;
+
+        camera = new OrthographicCamera(FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
+        camera.position.set(FRUSTUM_WIDTH / 2f, FRUSTUM_HEIGHT / 2f, 0);
     }
 
     @Override
-    protected void processEntity(Entity entity, float deltaTime) {
+    public void update(float deltaTime){
+        super.update(deltaTime);
 
+        //sort the render queue based on Z index (Depth)
+        renderQueue.sort(comparator);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.enableBlending();
+        batch.begin();
+
+        // looping through each entity on the render queue
+        for(Entity entity : renderQueue){
+            TextureComponent textureComp = textureMapper.get(entity);
+            TransformComponent transformComp = transformMapper.get(entity);
+
+            //nothing exists within this component, so there's nothing to load
+            if(textureComp.region == null || transformComp.isHidden){
+                continue;
+            }
+
+            float width = textureComp.region.getRegionWidth();
+            float height = textureComp.region.getRegionHeight();
+
+            //grbbing the center of our entity
+            float originX = width/2f;
+            float originY = height/2f;
+
+            batch.draw(textureComp.region,
+                    transformComp.position.x - originX, transformComp.position.y - originY,
+                    originX, originY,
+                    width, height,
+                    pixelsToMeters(transformComp.scale.x), pixelsToMeters(transformComp.scale.y),
+                    transformComp.rotation);
+        }
+
+        batch.end();
+        renderQueue.clear();
+    }
+
+    @Override
+    public void processEntity(Entity entity, float deltaTime) {
+        renderQueue.add(entity);
+    }
+
+    public OrthographicCamera getCamera(){
+        return camera;
     }
 }
