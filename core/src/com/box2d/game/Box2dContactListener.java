4bfd75157833e70bc1322b29226a2c50a10d72f4
wilkon <tb.wilkon@gmail.com>
2020-05-01 19:21:30 -0400

    ContactListener => now loading from ECS; no longer needs world

--- core/src/com/box2d/game/Box2dContactListener.java
+++ core/src/com/box2d/game/Box2dContactListener.java
@@ -1,16 +1,15 @@
 package com.box2d.game;
 
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.box2d.game.ces.components.CollisionComponent;
 import com.box2d.game.models.Box2dModel;
 
 public class Box2dContactListener implements ContactListener {
 
     private Box2dModel parent;
 
-    public Box2dContactListener(Box2dModel parent){
-        this.parent = parent;
+    public Box2dContactListener(){
     }
 
     @Override
@@ -20,39 +19,35 @@ public class Box2dContactListener implements ContactListener {
         Fixture fb = contact.getFixtureB();
         System.out.println(fa.getBody().getType() + " has hit " + fb.getBody().getType());
 
-        if (fa.getBody().getUserData() == "IAMTHESEA") {
-            parent.isSwimming = true;
+        if (fa.getBody().getUserData() instanceof Entity) {
+            Entity ent = (Entity) fa.getBody().getUserData();
+            entityCollision(ent, fb);
             return;
-        } else if (fb.getBody().getUserData() == "IAMTHESEA"){
-            parent.isSwimming = true;
+        } else if (fb.getBody().getUserData() instanceof Entity){
+            Entity ent = (Entity) fb.getBody().getUserData();
+            entityCollision(ent, fa);
             return;
-        }
-
-        if(fa.getBody().getType() == BodyType.StaticBody){
-            this.trampoline(fa, fb);
-        }else if(fb.getBody().getType() == BodyType.StaticBody){
-            this.trampoline(fb, fa);
         }
     }
 
-    private void trampoline(Fixture staticFixture, Fixture otherFixture){
-        System.out.println("force up");
-        otherFixture.getBody().applyForceToCenter(new Vector2(-1000, -1000), true);
-        parent.playSound(Box2dModel.BOING_SOUND);
+    private void entityCollision(Entity ent, Fixture fb){
+        if(fb.getBody().getUserData() instanceof Entity){
+            Entity fbEntity = (Entity) fb.getBody().getUserData();
+
+            CollisionComponent entityCol = ent.getComponent(CollisionComponent.class);
+            CollisionComponent fbCol = fbEntity.getComponent(CollisionComponent.class);
+
+            if(entityCol != null){
+                entityCol.collisionEntity = fbEntity;
+            }else if(fbCol != null){
+                fbCol.collisionEntity = ent;
+            }
+        }
     }
 
     @Override
     public void endContact(Contact contact) {
-        System.out.println("Contact!");
-        Fixture fa = contact.getFixtureA();
-        Fixture fb = contact.getFixtureB();
-        if(fa.getBody().getUserData() == "IAMTHESEA"){
-            parent.isSwimming = false;
-            return;
-        }else if(fb.getBody().getUserData() == "IAMTHESEA"){
-            parent.isSwimming = false;
-            return;
-        }
+        System.out.println("contact end");
     }
 
     @Override
